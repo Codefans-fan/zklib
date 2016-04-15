00a636956b32796e92683b1b80a2ed958366b353
fky <562867448@qq.com>
2016-04-15 16:54:09 +0800

    -remove print

--- zklib/zklib.py
+++ zklib/zklib.py
@@ -65,7 +65,6 @@ class ZKLib:
         buf = unpack('8B'+'%sB' % len(command_string), buf+command_string)
           
         chksum = unpack('H', self.createChkSum(buf))[0]
-        print chksum
         #print unpack('H', self.createChkSum(buf))
         reply_id += 1
         if reply_id >= USHRT_MAX:
