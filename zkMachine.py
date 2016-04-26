39b1ecb487031e34301b4c1c3a27e57940a7a9ff
fky <562867448@qq.com>
2016-04-26 10:25:28 +0800

    -add method to disconnect the machine.

--- zkMachine.py
+++ zkMachine.py
@@ -15,16 +15,22 @@ def addRecord(record):
 
 
 zk = zklib.ZKLib("172.69.8.4", 4370)
-
 res = zk.connect()
+
+
+temp = 0 # flag to disconnect
 if res:
-    while True:
+    while temp < 4:
         data = zk.zkrecvCurrentAtt()   #(uid, time)
         if data:
             if type(data) == types.BooleanType:
                 continue
             t = threading.Thread(target=addRecord,args=(data,))
             t.start()
+            temp += 1
+
+
+
 
 
 #zk.disconnect()
